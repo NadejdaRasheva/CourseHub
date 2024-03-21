57488a7b823a8570b1221db632450f9668e6518d
NadejdaRasheva <114023419+NadejdaRasheva@users.noreply.github.com>
2024-03-21 18:28:02 +0200

    Creating layers and the database

--- CourseHub.Infrastructure/Data/ApplicationDbContext.cs
+++ CourseHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace CourseHub.Data
+namespace CourseHub.Infrastrucure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- CourseHub/Program.cs
+++ CourseHub/Program.cs
@@ -1,4 +1,4 @@
-using CourseHub.Data;
+using CourseHub.Infrastrucure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
