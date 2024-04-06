b2e60d0d3acc514b084b12a970a28e820078ad91
NadejdaRasheva <114023419+NadejdaRasheva@users.noreply.github.com>
2024-04-06 17:49:25 +0300

    Custom Error Pages added

--- CourseHub/Controllers/HomeController.cs
+++ CourseHub/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using CourseHub.Core.Models.Home;
 using CourseHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -20,11 +22,21 @@ namespace CourseHub.Controllers
             return View(model);
         }
 
-
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
