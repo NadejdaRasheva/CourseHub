f6392d44bedd885a522d7f2f163f8c8bd0a023ba
NadejdaRasheva <114023419+NadejdaRasheva@users.noreply.github.com>
2024-04-14 14:31:49 +0300

    Teacher service fixed

--- CourseHub.Core/Services/TeacherService.cs
+++ CourseHub.Core/Services/TeacherService.cs
@@ -21,18 +21,17 @@ namespace CourseHub.Core.Services
 
         public async Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
         {
-            return await _data.Teachers.AllAsync(t => t.PhoneNumber == phoneNumber);
+            return await _data.Teachers.AnyAsync(t => t.PhoneNumber == phoneNumber);
         }
 
         public async Task CreateAsync(string userId, string phoneNumber)
         {
-            var teacher = new Teacher()
+			await _data.Teachers.AddAsync(new Teacher()
             {
                 UserId = userId,
                 PhoneNumber = phoneNumber
-            };
+            });
 
-            await _data.Teachers.AddAsync(teacher);
             await _data.SaveChangesAsync();
         }
 
