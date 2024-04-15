8e0ed4e22d02bc310008add80c3add06fac00c7d
NadejdaRasheva <114023419+NadejdaRasheva@users.noreply.github.com>
2024-04-16 00:57:51 +0300

    Design improved

--- CourseHub/Controllers/HomeController.cs
+++ CourseHub/Controllers/HomeController.cs
@@ -33,17 +33,22 @@ namespace CourseHub.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-            if (statusCode == 400)
-            {
-                return View("Error400");
-            }
-
             if (statusCode == 401)
             {
                 return View("Error401");
             }
 
-            return View();
+            if (statusCode == 404)
+            {
+	            return View("Error404");
+            }
+
+            if (statusCode == 500)
+            {
+	            return View("Error500");
+            }
+
+			return View();
         }
     }
 }
